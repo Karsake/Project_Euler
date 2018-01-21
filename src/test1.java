fffaf8dd731c0131c4fe04ca74b9f84eb50a4183
Karsake <qianshangsam@gmail.com>
2018-01-21 11:15:32 +0800

    2

--- src/test1.java
+++ src/test1.java
@@ -1,4 +1,7 @@
-
+//	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+//	The sum of these multiples is 23.
+//
+//	Find the sum of all the multiples of 3 or 5 below 1000.
 public class test1 {
 	public static void main(String[] args) {
 		int x,y,z;
@@ -6,7 +9,7 @@ public class test1 {
 		y=getSum(5,1000);
 		z=getSum(15,1000);
 		System.out.println(x+y-z);
-		//Êä³ö233168
+		//Output 233168
 	}
 
 	static int getSum(int a,int b) {
